e01498ed500936fd930501dc5bfe83e385b5700e
adriandero <adrian-nikola@outlook.com>
2024-08-29 11:56:54 +0200

    Delete unessessary files and Add new Contact and Footer section to page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,13 @@ import { createContext } from "react";
 
 import { Button, NextUIProvider } from "@nextui-org/react";
 import NavBar from "./components/NavBar/NavBar";
+import Footer from "./components/Footer/Footer";
 
 import "./page.css";
 import MainFrame from "./components/MainFrame/MainFrame";
 import StorySection from "./components/StorySection/StorySection";
 import WorkSection from "./components/WorkSection/WorkSection";
+import ContactSection from "./components/ContactSection/ContactSection";
 
 export default function Home() {
   const scrollToJourney: () => {} = () => {
@@ -24,11 +26,18 @@ export default function Home() {
     return {};
   };
 
+  const scrollToContact: () => {} = () => {
+    const ContactSection = document.getElementById("contact-section")!;
+    ContactSection.scrollIntoView({ behavior: "smooth" });
+    return {};
+  };
+
   return (
     <NextUIProvider className="flex items-center flex-col min-h-screen">
       <NavBar
         scrollToJourney={scrollToJourney}
         scrollToWork={scrollToWork}
+        scrollToContact={scrollToContact}
       ></NavBar>
       <main className="flex flex-col max-w-screen-xl w-full h-full items-center dark text-foreground bg-background">
         <MainFrame scrollToJourney={scrollToJourney} />
@@ -46,7 +55,17 @@ export default function Home() {
           </span>
         </h1>
         <WorkSection />
+        <h1 className="text-4xl font-semibold py-6 mt-32" id="contact-section">
+          What&apos;s{" "}
+          <span className="bg-gradient-to-r from-cyan-600 to-blue-600 font-bold text-transparent bg-clip-text">
+            Next
+          </span>{" "}
+          ?
+        </h1>
+        <ContactSection />
       </main>
+
+      <Footer />
     </NextUIProvider>
   );
 }
