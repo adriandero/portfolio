5ebccd6680d74111483153e4e2df513c3f8d47e1
adriandero <adrian-nikola@outlook.com>
2024-08-29 11:56:10 +0200

    Add footer section

---
+++ src/app/components/Footer/Footer.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Button, NextUIProvider } from "@nextui-org/react";
+
+import { Icon } from "react-icons-kit";
+import { arrowRight } from "react-icons-kit/feather/arrowRight";
+import { minus } from "react-icons-kit/feather/minus";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRightLong } from "@fortawesome/free-solid-svg-icons";
+
+interface PropsInterface {}
+
+export default function Footer({}: PropsInterface) {
+  return <footer className=""></footer>;
+}
